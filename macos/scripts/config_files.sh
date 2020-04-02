fee1220a1afed311ffe9170ec3754bd1bf239c5c
davidoort <davidoort@hotmail.com>
2020-04-02 11:31:19 +0200

    writing to inputrc ready to test

--- macos/scripts/config_files.sh
+++ macos/scripts/config_files.sh
@@ -6,16 +6,15 @@ set -x
 echo -e 'bash ~/.aliases' >> ~/.bash_profile
 
 # .aliases
-
 ## git status
 echo -e 'alias gs="git status"' >> ~/.aliases
 
 
-
 # .inputrc
-
 # Incremental history searching
-    "\e[A": history-search-backward
-    "\e[B": history-search-forward
-    "\e[C": forward-char
-    "\e[D": backward-char
+cat > ~/.inputrc << "EOF"
+"\e[A": history-search-backward
+"\e[B": history-search-forward
+"\e[C": forward-char
+"\e[D": backward-char
+EOF
