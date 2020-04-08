c738d6cf46d48a631509b2c5c604170f8bd33102
David Oort Alonso <davidoort@hotmail.com>
2020-04-08 10:08:37 +0200

    added android paths to bash_profile

--- macos/scripts/config_files.sh
+++ macos/scripts/config_files.sh
@@ -7,7 +7,8 @@ echo -e 'source ~/.aliases' >> ~/.bash_profile
 export PATH="/usr/local/bin:$PATH"
 export PATH="$PATH:`pwd`/flutter/bin"
 export PATH="$PATH:`pwd`/flutter/bin/cache/dart-sdk/bin"
-
+export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
+export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools:$PATH
 # .aliases
 cp ~/setup/macos/files/.aliases ~/.aliases
 
